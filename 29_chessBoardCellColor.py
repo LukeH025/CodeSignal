d7b16648eff91cb18ee35d5ac4eee5530fa9de17
Luke <lukehenderick@gmail.com>
2023-10-28 22:49:31 -0400

    Añadido 42

--- 29_chessBoardCellColor.py
+++ 29_chessBoardCellColor.py
@@ -3,7 +3,6 @@ def solution(cell1, cell2):
     alphabet_upper=list(string.ascii_uppercase)
     letter1=alphabet_upper.index(cell1[0])+1+int(cell1[1])
     letter2=alphabet_upper.index(cell2[0])+int(cell2[1])
-    print(ord(cell1[0]))
     if letter1%2==letter2%2:
         return True
     else:
---
+++ 42_Bishop and Pawn.py
@@ -0,0 +1,11 @@
+def solution(bishop, pawn):
+    letters=["a","b","c","d","e","f","g","h"]
+    inicio=(letters.index(bishop[0]))
+    final=(letters.index(pawn[0])) 
+    dist=final-inicio
+    # print(int(bishop[1])+dist)
+    if int(bishop[1])+dist==int(pawn[1]) or int(bishop[1])-dist==int(pawn[1]):
+        return True
+    else:return False
+
+print(solution("a1","f4"))
